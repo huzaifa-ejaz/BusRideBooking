de278971462c8f1d6f854bce4dd9a5c1bd5c574f
huzaifa-ejaz <57786303+huzaifa-ejaz@users.noreply.github.com>
2019-12-02 02:49:10 +0500

    Update book-customer-email.php

--- book-customer-email.php
+++ book-customer-email.php
@@ -1,7 +1,15 @@
+<?php
+    require 'dbconfig.php';
+?>
+
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Expresso|Book My Ride</title>
+        <title>Bussed|Book My Ride</title>
         <link rel="stylesheet" type="text/css" href="styles/navbar-styles.css">
         <link rel="stylesheet" type="text/css" href="styles/footer-style.css">
         <link rel="stylesheet" type="text/css" href="styles/forms-styles.css">
@@ -51,12 +59,87 @@
 	  <br>
 	  <br>
 	  <div class="w3-panel w3-border w3-round-xxlarge">
-        <form>
-           <p>Email:<br>
-             <input class="input" type="text" name:"cemail" size="20" maxlength="35"/>
-           </p> 
+        <form method="post">
+           Email:
+             <input type="email" name="cemail" size="30" maxlength="35"/>
+           
+           <input type="submit" value="Book Now" name="giveemail" class="book-btn">
         </form>
-        <a href="book-customer-info.html"><button class= "button">Go!</button></a>
+        <?php
+                  
+                   if (isset($_POST["giveemail"])) {
+                       $email = $_POST["cemail"];
+                       $_SESSION["email"] = $email;
+                       $ride_id = $_SESSION["ride_id"];
+                      
+                       $get_cr_id = "select cr_id from customer where email='$email'";
+                       $result = mysqli_query($con,$get_cr_id);
+                       if(!$result) {
+                         exit("Query couldn't be executed");
+                       }
+                       if(mysqli_num_rows($result) > 0){
+                             $row = mysqli_fetch_assoc($result);
+                             $cr_id = $row["cr_id"];
+
+                             $check_cr_id = "select * from booking where cr_id=$cr_id";
+                             $resultchk = mysqli_query($con,$check_cr_id);
+                             if (!$resultchk) {
+                              exit("Query couldn't be executed");
+                             }
+                             $rows_cleared = 0;
+                             if(mysqli_num_rows($resultchk) === 0) {
+                                  $query_booking = "insert into booking(cr_id,ride_id,status) values ('$cr_id','$ride_id','booked')";
+                                  $result = mysqli_query($con,$query_booking);
+                                  if(!$result) {
+                                     exit("Query could not be executed") ;
+                                   }
+        
+                                   $result = mysqli_query($con,"select bk_id from booking where cr_id='$cr_id' and ride_id='$ride_id' ");
+                                   $row = mysqli_fetch_assoc($result);
+                                   $bk_id = $row['bk_id'];
+                                   $_SESSION["bk_id"] = $bk_id;
+
+                                   header("location:booked-customer-ride.php");
+                                  } else {
+                                    while($row = mysqli_fetch_assoc($resultchk)) {
+                                      $can = strcmp($row['status'],"cancelled");
+                                      $tak = strcmp($row['ride_taken'],"taken");
+                                      if($can === 0 or $tak === 0) {
+                                        $rows_cleared++;
+                                      }
+                                    }
+                                    if($rows_cleared === mysqli_num_rows($resultchk)) {
+                                                $query_booking = "insert into booking(cr_id,ride_id,status) values ('$cr_id','$ride_id','booked')";
+                                                $result = mysqli_query($con,$query_booking);
+                                                if(!$result) {
+                                                       exit("Query could not be executed") ;
+                                                 }
+        
+                                               $result = mysqli_query($con,"select bk_id from booking where cr_id='$cr_id' and ride_id='$ride_id' ");
+                                               $row = mysqli_fetch_assoc($result);
+                                               $bk_id = $row['bk_id'];
+                                                $_SESSION["bk_id"] = $bk_id;
+
+                                                header("location:booked-customer-ride.php");
+                                    } else {
+                                      echo '
+                                            <div><p> You already have a booked ride! </p></div>
+                                           ';
+                                    }
+                                  }
+                       
+                       }else {
+                         header("location:book-customer-info.php");
+                       }
+                      }
+
+
+
+
+
+
+                    
+                ?>
 		</div>
     </body>
 </html>
