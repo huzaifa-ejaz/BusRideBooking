605480b6a6308ccb28851ad09b13454b5d9b7c01
huzaifa-ejaz <57786303+huzaifa-ejaz@users.noreply.github.com>
2019-12-02 09:54:40 +0500

    Update booked-customer-ride.php

--- booked-customer-ride.php
+++ booked-customer-ride.php
@@ -1,3 +1,11 @@
+<?php
+// Start the session
+session_start();
+?>
+<?php
+    require 'dbconfig.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -21,17 +29,112 @@
                         
                 </ul>
         <h2>My Booked Ride<h2>
-        <div class="ride-detail" id="booked-ride">
-                <div class="od-line"></div>
-                <div class="origin-box">Karachi</div>
-                <div class="dest-box">Lahore</div>
-                <div class="fare-box">2000PKR</div>
-                <div class="date-box">20/Nov/2019</div>
-                <div class="time-box">12:30PM</div>
-                <div class="seat-box">5 Seats Booked</div>
-                <img src="images/money-icon.png" class="money-icon">
-    
-                <a href="booked-cancelled.html"><button class="book-btn">Cancel Booking</button></a>
-            </div>
+        <?php
+                $email = $_SESSION["email"];
+                $bk_id = $_SESSION["bk_id"];
+
+                
+
+                $get_cr_id = "select cr_id from customer where email='$email'";
+                $result = mysqli_query($con,$get_cr_id);
+                if(!$result) {
+                     exit("Query could not be executed!");
+                }
+                $row = mysqli_fetch_assoc($result);
+                $cr_id_from_email = $row['cr_id'];
+
+                
+                
+
+                $get_cr_id2 = "select cr_id from booking where bk_id='$bk_id'";
+                $result = mysqli_query($con,$get_cr_id2);
+                if(!$result) {
+                     exit("Query could not be executed!");
+                }
+                $row = mysqli_fetch_assoc($result);
+                $cr_id_from_booking = $row['cr_id'];
+
+                
+                
+
+                if($cr_id_from_email = $cr_id_from_booking) {
+                       $get_ride_id = "select ride_id from booking where bk_id='$bk_id'";
+                       $result = mysqli_query($con,$get_ride_id);
+                       if(!$result) {
+                             exit("Query could not be executed!");
+                        }
+                        $row = mysqli_fetch_assoc($result);
+                        $ride_id = $row['ride_id'];
+                        
+                        $date_format = mysqli_real_escape_string($con,"%d/%m/%y");
+                        $time_format = mysqli_real_escape_string($con,"%l:%i %p");
+                        $get_ride = "select ri.ride_id,date_format(ri.start_time, '$date_format') startd, date_format(ri.start_time,'$time_format') startt, ri.price,ro.origin,ro.destination,bo.bk_id,bo.status from ride ri,route ro,booking bo where ri.ride_id='$ride_id' and ro.route_id = ri.route_id and bo.bk_id ='$bk_id'";
+                        $result = mysqli_query($con,$get_ride);
+                        if(!$result) {
+                             exit("Query could not be executed!");
+                         }
+                         $row = mysqli_fetch_assoc($result);
+                         echo '
+                              <div class="ride-detail" id="booked-ride">
+                                     <div class="od-line"></div>
+                                     <div class="origin-box">'.$row["origin"].'</div>
+                                     <div class="dest-box">'.$row["destination"].'</div>
+                                     <div class="fare-box">'.$row["price"].'PKR</div>
+                                     <div class="date-box">'.$row["startd"].'</div>
+                                     <div class="time-box">'.$row["startt"].'</div>
+                                     <div class="seat-box"> Booking ID: '.$row["bk_id"].'</div>
+                                     <img src="images/money-icon.png" class="money-icon">
+                                     <input type="hidden" value='.$row["ride_id"].' name=ride_id>
+                                     <div class="book-btn" class"status">'.$row["status"].'</div>
+                              </div>
+                              ';
+                              $status = $row["status"];
+                              $cmp = strcmp($status,"booked");
+
+                  
+                            if( $cmp === 0) {
+                              
+                               
+                               echo '
+                               <form method="post">
+                               <button onclick="return confirm(\'Are you sure you want to cancel this booking?\');" type="submit" name="cancelbooking" value="Cancel">Cancel Booking</button>
+                               <button onclick="return confirm(\'Are you sure you want to confirm this booking?\');" type="submit" name="confirmbooking" value="Confirm">Confirm Booking</button>
+                               </form>
+                               ';
+                               
+                               if(isset($_POST["cancelbooking"])) {
+                                  $query = "update booking set status='cancelled' where bk_id='$bk_id'";
+                                  $result = mysqli_query($con,$query);
+                                  if(!$result) {
+                                     exit("Query could not be executed!");
+                                  }
+
+                                  header("location:email-sent.html");
+                               }
+                               if(isset($_POST["confirmbooking"])) {
+                                  $query = "update booking set status='confirmed' where bk_id='$bk_id'";
+                                  $result = mysqli_query($con,$query);
+                                  if(!$result) {
+                                      exit("Query could not be executed!");
+                                  }
+
+                                header("location:email-sent.html");
+                               }
+                          }
+                          
+                            
+                }else {
+                  echo ' 
+                      <div>
+                      <p>We could not find your booking!</p>
+                      <a href="booked-customer-info.php"><button type="button">Check Again</button></a>
+                      <a href="book-ride-start-end.php"><button type="button">Book A New Ride</button></a>
+                      </div>
+                  
+                  
+                  ';
+                }
+              
+            ?>
     </body>
 </html>
